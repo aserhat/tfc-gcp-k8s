c9d929eac97ba4a6f3f5778a92b350b7793798e3
Allen Serhat <serhata@gmail.com>
2020-04-03 00:30:15 -0400

    Downsize

--- main.tf
+++ main.tf
@@ -8,20 +8,20 @@ provider "google" {
 module "etcd" {
   source       = "./modules/create-gce-vm"
   server_name_prefix  = "etcd"
-  server_count = 1
+  server_count = 0
   server_machine_type = "f1-micro"
 }
 
 module "masters" {
   source       = "./modules/create-gce-vm"
   server_name_prefix  = "masters"
-  server_count = 1
+  server_count = 0
   server_machine_type = "f1-micro"
 }
 
 module "nodes" {
   source       = "./modules/create-gce-vm"
   server_name_prefix  = "nodes"
-  server_count = 1
+  server_count = 0
   server_machine_type = "f1-micro"
 }

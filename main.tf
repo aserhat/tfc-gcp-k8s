dc20a58c25fdb389f5e7ead16c01318d968b0681
Allen Serhat <serhata@gmail.com>
2020-04-03 22:02:17 -0400

    Downsize

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "etcd" {
   source       = "./modules/create-gce-vm"
   server_name_prefix  = "etcd"
-  server_count = 1
+  server_count = 0
   server_machine_type = "f1-micro"
   gcp_creds = "${var.gcp_creds}"
 }
@@ -9,7 +9,7 @@ module "etcd" {
 module "masters" {
   source       = "./modules/create-gce-vm"
   server_name_prefix  = "masters"
-  server_count = 1
+  server_count = 0
   server_machine_type = "f1-micro"
   gcp_creds = "${var.gcp_creds}"
 }
@@ -17,7 +17,7 @@ module "masters" {
 module "nodes" {
   source       = "./modules/create-gce-vm"
   server_name_prefix  = "nodes"
-  server_count = 1
+  server_count = 0
   server_machine_type = "f1-micro"
   gcp_creds = "${var.gcp_creds}"
 }

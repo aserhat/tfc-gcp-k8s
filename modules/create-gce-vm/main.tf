8a5be1bf9fafd56007ab2f60c2c5a29c6310937f
Allen Serhat <serhata@gmail.com>
2020-04-04 23:41:03 -0400

    Update module

--- modules/create-gce-vm/main.tf
+++ modules/create-gce-vm/main.tf
@@ -2,6 +2,7 @@ resource "google_compute_instance" "vm_instance" {
   count        = "${var.server_count}"
   name         = "${var.server_name_prefix}-0${count.index}"
   machine_type = "${var.server_machine_type}"
+  allow_stopping_for_update = true
 
   tags = "${var.machine_tags}"
 

744f9bdf5f42ef2117a2df5c5abda3072f443244
Allen Serhat <serhata@gmail.com>
2020-04-04 22:43:03 -0400

    Formatting

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 module "etcd" {
-  source       = "./modules/create-gce-vm"
+  source              = "./modules/create-gce-vm"
   server_name_prefix  = "etcd"
-  server_count = 0
+  server_count        = 0
   server_machine_type = "f1-micro"
-  gcp_creds = "${var.gcp_creds}"
+  gcp_creds           = "${var.gcp_creds}"
 }
 
 module "masters" {
-  source       = "./modules/create-gce-vm"
+  source              = "./modules/create-gce-vm"
   server_name_prefix  = "masters"
-  server_count = 0
+  server_count        = 0
   server_machine_type = "f1-micro"
-  gcp_creds = "${var.gcp_creds}"
+  gcp_creds           = "${var.gcp_creds}"
 }
 
 module "nodes" {
-  source       = "./modules/create-gce-vm"
+  source              = "./modules/create-gce-vm"
   server_name_prefix  = "nodes"
-  server_count = 0
+  server_count        = 0
   server_machine_type = "f1-micro"
-  gcp_creds = "${var.gcp_creds}"
+  gcp_creds           = "${var.gcp_creds}"
 }
